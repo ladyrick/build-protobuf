f6c6761e5a2abb8ee0cd6875fe79b47319ebbc17
LadyRick <ladyrick@qq.com>
2018-08-27 11:38:11 +0800

    Create build-protobuf.sh

---
+++ build-protobuf.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+PROTOBUF_ROOT=$1
+if [ ! $PROTOBUF_ROOT ]
+then
+    echo need protobuf root path
+    exit -1
+fi
+
+PROTOBUF_ROOT_BASENAME=$(basename $PROTOBUF_ROOT)
+BUILD_DIR=$(pwd)/${PROTOBUF_ROOT_BASENAME//[ -\.]/_}
+
+if [ ! "${BUILD_DIR/ /}" = "$BUILD_DIR" ]
+then
+    echo current folder path contains white space.
+    exit -1
+fi
+
+echo delete old build dir: $BUILD_DIR
+rm -rf $BUILD_DIR
+
+cd $PROTOBUF_ROOT
+make clean
+./configure CFLAGS="-fPIC" CXXFLAGS="-fPIC" --prefix=${BUILD_DIR}/usr/local/lib --enable-shared=no
+make -j8
+make install # this command will install protobuf to ${BUILD_DIR}/usr/local/lib
+
+mv ${BUILD_DIR}/usr/local/lib/* ${BUILD_DIR}
+rm -rf ${BUILD_DIR}/usr
